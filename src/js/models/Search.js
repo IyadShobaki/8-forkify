f3450e907ac35aad15c833be926c1113d70439b8
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-27 16:57:29 -0400

    Building the Search Model

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,2 +1,19 @@
-// const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${KEY}&q=${this.query}`);  replace with the following
-//const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+    async getResults(){
+        // Different from fetch, axios will return json. With fetch we have to convert it to json
+        try{
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
+            this.result = res.data.recipes; 
+            //console.log(this.result);
+            
+        } catch (error) {
+            alert(error)
+        }
+    }
+}
+
