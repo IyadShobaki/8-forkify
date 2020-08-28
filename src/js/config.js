49953d254e7433a806a7e18bb890a37b0306fb58
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-28 12:14:49 -0400

    Building the Recipe Model - Part 1

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+// In case if we have to use proxy or API key
+// export const proxy = 'https..........';
+// export const key = 'API key'
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,3 +1,34 @@
-//const res = await axios(`${PROXY}http://food2fork.com/api/get?key=${KEY}&rId=${this.id}`);
-// replace with the following
-// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+import axios from 'axios';
+
+export default class Recipe {
+    constructor(id){
+        this.id = id;
+    }
+    
+    async getRecipe() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
+
+            //console.log(res);
+        }catch (error){
+            console.log(error);
+            alert('Something went wrong :(');
+        }
+    }
+
+    calcTime() {
+        // Assuming that we need 15 min for each 3 ingredients
+        const numberOfIngredients = this.ingredients.length;
+        const periods = Math.ceil(numberOfIngredients / 3);
+        this.time = periods * 15;
+
+    }
+    calcServings() {
+        this.servings = 4;
+    }
+}
