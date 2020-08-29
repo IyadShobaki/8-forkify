e8fbe0c9adab8bf33a0641568f926379765aa3f7
IyadShobaki <iyadshobaki82@gmail.com>
2020-08-28 22:02:08 -0400

    Implementing Persistent Data with localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -154,9 +154,6 @@ elements.shopping.addEventListener('click', e => {
     }
 });
 
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 
 // LIKES CONTROLLER
 const controlLike = () => {
@@ -195,6 +192,19 @@ const controlLike = () => {
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 }
 
+// Restore liked recipes on page loads
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+});
 
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
